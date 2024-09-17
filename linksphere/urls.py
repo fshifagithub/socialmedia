fe273cff818a08556379914c7ba43e83ecf734a9
fathimashifa <fathimashifasalim@gmail.com>
2024-09-17 22:01:29 +0530

    final

--- linksphere/urls.py
+++ linksphere/urls.py
@@ -16,7 +16,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from social import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path("register",views.SignUpView.as_view(),name="signup"),
+    path("",views.SignInView.as_view(),name="signin"),
+    path("index",views.IndexView.as_view(),name="index"),
+    path("logout",views.SignOutView.as_view(),name='signout'),
+    path("profiles/<int:pk>/change",views.ProfileUpdateView.as_view(),name="profile-edit"),
+    path("profiles/<int:pk>",views.ProfileDetailView.as_view(),name="profile-detail"),
+    path("profiles/all",views.ProfileListView.as_view(),name="profile-list"),
+    path("profile/<int:pk>/follow",views.FollowView.as_view(),name="follow"),
+    path("post/<int:pk>/like",views.PostLikeView.as_view(),name="like"),
+    path("post/<int:pk>/comments/add",views.CommentView.as_view(),name="comment"),
+    path("profile/<int:pk>/block",views.ProfileBlockView.as_view(),name="block"),
+    path("stories/add",views.StoriesCreateView.as_view(),name="story-create")
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- social/admin.py
+++ social/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from social.models import Stories,Posts
+
+admin.site.register(Stories)
+admin.site.register(Posts)
